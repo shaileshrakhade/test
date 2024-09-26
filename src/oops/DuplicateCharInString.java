84d9bb561e5bb0e31abc9aa919d614f1a0de5980
Shailesh Rakhade <shailesh15.sr@gmail.com>
2024-09-26 20:54:12 +0530

    practice

---
+++ src/oops/DuplicateCharInString.java
@@ -0,0 +1,28 @@
+package oops;
+
+public class DuplicateCharInString {
+    //find duplicate char in String using only one for loop
+    public static void main(String[] args) {
+        String s = "abcxyzab";
+        for (int i = 0; i < s.length(); i++) {
+                isDuplicate(s, i, s.charAt(i));
+        }
+    }
+
+    static char isDuplicate(String s, int i, char c) {
+        i++;
+        if (i < s.length()) {
+
+//            System.out.println(c + "::" + s.charAt(i));
+            if (c != s.charAt(i))
+                isDuplicate(s, i, c);
+            if (c == s.charAt(i)) {
+                System.out.println(c);
+                return c;
+            }
+
+
+        }
+        return ' ';
+    }
+}
